affac526a4200ae728cdcc3affbdce6f144cc58e
Matthew Collier <collier.matthew@gmail.com>
2016-07-28 11:23:00 -0400

    Use shorthand require.

--- run.js
+++ run.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
-require('./lib/index');
+require('./lib');
 
 // run with development defaults
 bedrock.start();
